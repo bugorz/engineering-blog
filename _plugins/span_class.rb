9b492ce2925f0a4f32e1eecc2d9f1d95f9c5c169
T.J. Corrigan <tjcorr@monsanto.com>
2016-03-14 09:20:29 -0500

    publishing master 7448362c0c1dc01ac09e205b8296b70fde116c32 build to gh-pages on Mon Mar 14 09:20:29 CDT 2016

--- _plugins/span_class.rb
+++
@@ -1,9 +0,0 @@
-module Jessitron
-  module SpanWithClass
-    def sc(input, second_arg)
-      "<span class=\"#{second_arg}\">#{input}</span>"
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jessitron::SpanWithClass)
